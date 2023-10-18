6f3f64618ab300fdfdc04608afd5e6519f48a52f
Didier Guerrero <didier.guerrero@udla.edu.ec>
2023-10-18 07:36:03 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DidierGuerrero_TallerMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
